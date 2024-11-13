d9f78f1b4e12730b8ecda795eef681c541de7d8b
Victor Shumskiy <krishna2003@gmail.com>
2024-11-13 17:46:56 +0300

    Add files via upload

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,35 @@
-output "count" {
-  value = [
-    for vm in yandex_compute_instance.web : {
-      name = vm.name
-      id   = vm.id
-      fqdn = vm.fqdn
-    }
-  ]
-}
+# output "count" {
+#   value = [
+#     for vm in yandex_compute_instance.web : {
+#       name = vm.name
+#       id   = vm.id
+#       fqdn = vm.fqdn
+#     }
+#   ]
+# }
 
-output "for_each" {
-  value = [
-    for vm in yandex_compute_instance.databases : {
+# output "for_each" {
+#   value = [
+#     for vm in yandex_compute_instance.databases : {
+#       name = vm.name
+#       id   = vm.id
+#       fqdn = vm.fqdn
+#     }
+#   ]
+# }
+
+#обьеденил при помощи concat
+output "all_vms" {
+  value = concat(
+    [for vm in yandex_compute_instance.web : {
       name = vm.name
       id   = vm.id
       fqdn = vm.fqdn
-    }
-  ]
+    }],
+    [for vm in yandex_compute_instance.databases : {
+      name = vm.name
+      id   = vm.id
+      fqdn = vm.fqdn
+    }]
+  )
 }
